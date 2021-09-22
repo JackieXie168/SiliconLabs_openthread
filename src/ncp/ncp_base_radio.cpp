71a1add80c53c436dad9f809f217f1ab9bf42d83
Hemanth Rao <hemanth.laks@silabs.com>
2021-09-22 21:59:36 +0100

    Added mIid to RadioFrames

--- src/ncp/ncp_base_radio.cpp
+++ src/ncp/ncp_base_radio.cpp
@@ -467,6 +467,8 @@ otError NcpBase::HandlePropertySet_SPINEL_PROP_STREAM_RAW(uint8_t aHeader)
 
         SuccessOrExit(error = DecodeStreamRawTxRequest(*frame));
 
+        frame->mIid = mCurCommandIID;
+
         // Pass frame to the radio layer. Note, this fails if we
         // haven't enabled raw stream or are already transmitting.
         SuccessOrExit(error = otLinkRawTransmit(mInstance, &NcpBase::LinkRawTransmitDone));
