a9753e7b5e6e47bf4687060c252ac511b62e04b6
Bob MacDonald <Robert.MacDonald@silabs.com>
2021-11-05 11:27:35 -0400

    While working on OPENTHREAD-1124 I discovered that there were some descrepencies between GitHub OpenThread and our stash OpenThread so I compared the devel/21q4 branch to the master branch and looked at all the diffs.  I expected diffs associated with multipan and coprocessor cli support so I ignored those differences.  However, there were some differences that need to be fixed and I'm addressing them in this commit.  These were most likely introduced by bad merges.
    This is the URL I used to compare the branches.  You can easily do this using the compare option in stash.
    
    https://stash.silabs.com/projects/WMN_STACKS/repos/openthread/compare/diff?targetBranch=refs%2Fheads%2Fmaster&sourceBranch=refs%2Fheads%2Fdevelop%2F21q4&targetRepoId=2181
    
    Suvesh also informed me that is can be done this way:
    
    I do this on my branch like so (finds the common parent with GitHub master to compare to, in case master is ahead since we last merged):
    
    git merge-base $(git rev-parse --abbrev-ref HEAD) origin/master | xargs -I{} git diff {}

--- src/core/common/settings.hpp
+++ src/core/common/settings.hpp
@@ -733,7 +733,7 @@ protected:
 #if OPENTHREAD_CONFIG_LOG_UTIL && (OPENTHREAD_CONFIG_LOG_LEVEL >= OT_LOG_LEVEL_WARN)
     static const char *KeyToString(Key aKey);
 #endif
-#if OPENTHREAD_CONFIG_LOG_UTIL && (OPENTHREAD_CONFIG_LOG_LEVEL >= OT_LOG_LEVEL_WARN)
+#if OPENTHREAD_CONFIG_LOG_UTIL && (OPENTHREAD_CONFIG_LOG_LEVEL >= OT_LOG_LEVEL_INFO)
     static const char *ActionToString(Action aAction);
 #endif
 };
